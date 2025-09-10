688077ab58b1b41cccc9ef31b00d9fc6a8cdb0c6
Manvender <manvender.meena.mat21@itbhu.ac.in>
2025-09-10 11:09:56 +0530

    conflict resolved

--- include/utils.h
+++ include/utils.h
@@ -10,20 +10,11 @@ std::string Hash(const std::string &content);
 std::string getCurrentTime();
 
 // Global paths
-<<<<<<< HEAD
 extern const std::string Code_Vault;
 extern const std::string Objects;
 extern const std::string Commits;
 extern const std::string Head_File;
 extern const std::string Index_File;
 extern const std::string Branch_File;
-=======
-extern const std::string MINI_GIT_DIR;
-extern const std::string OBJECTS_DIR;
-extern const std::string COMMITS_DIR;
-extern const std::string HEAD_FILE;
-extern const std::string INDEX_FILE;
-extern const std::string BRANCHES_FILE;
->>>>>>> 4d7a456ab10636be5fc14449d2fb2c4c38270d76
 
 #endif // UTILS_H
